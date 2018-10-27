c4420882cafb7d0f5afb6b91b7b1c96aacd4d827
Mahidhar Nandigam <mahidhar.nandigam@gmail.com>
2018-10-27 14:18:09 -0400

    code from spring initializr

---
+++ src/main/java/com/company/project/masterdata/MasterDataApplication.java
@@ -0,0 +1,12 @@
+package com.company.project.masterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterDataApplication.class, args);
+	}
+}
